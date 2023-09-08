c859b2d3ac7b37c69adae8c141e7c273ba8382fd
Trupti100 <127935389+Trupti100@users.noreply.github.com>
2023-09-08 09:50:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const isPowerOfTwo = (number) => {
-  //your code here
-};
+function isPowerOfTwo(number) {
+  // Check if the number is greater than 0 and has only one set bit (binary representation contains a single '1')
+  return number > 0 && (number & (number - 1)) === 0;
+}
 
-const num = parseInt(prompt("Enter a number"));
-alert(isPowerOfTwo(num));
+// Test cases
+console.log(isPowerOfTwo(1024)); // Output: true
+console.log(isPowerOfTwo(100));  // Output: false
+console.log(isPowerOfTwo(0));    // Output: false
+console.log(isPowerOfTwo(-2));   // Output: false
